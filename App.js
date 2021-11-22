75eed80b656f0b008791e2d62cbb380485a12109
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2021-11-22 14:36:27 -0500

    List Added
    Displays a list of shopping list items and includes the ability to delete the item by hitting the red x.

--- App.js
+++ App.js
@@ -1,24 +1,38 @@
 //Main components... All other components will be under APP.
 import React, {useState} from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 //UUID for id generation
-//import {uuid} from 'uuidv4';
+import uuid from 'react-native-uuid';
 
 //Components 
 import Header from "./components/Header";
+import ListItem from "./components/ListItem";
 
 const App = () => {
   const [items, setItems] = useState([
-    {id: Math.round(), text: 'Milk'},
-    {id: Math.round(), text: 'Eggs'},
-    {id: Math.round(), text: 'Yogurt'},
-    {id: Math.round(), text: 'Cheese'},
+    {id: uuid.v4(), text: 'Milk'},
+    {id: uuid.v4(), text: 'Eggs'},
+    {id: uuid.v4(), text: 'Yogurt'},
+    {id: uuid.v4(), text: 'Cheese'},
   ]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  }
 
   return (
     <View style={styles.container}>
       <Header title='Shopping List'/>
+      <FlatList
+       data={items}
+       renderItem={({item}) => (
+         <ListItem item={item} deleteItem={deleteItem}/>
+       )}
+      //keyExtractor={item => item.id}
+      />
     </View>
   );
 };
---
+++ components/AddItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native'
+
+const AddItem = ({title}) => {
+  return (
+      <View></View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default AddItem;
