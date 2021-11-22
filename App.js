0fed0dbb14f077ace87f725cfdd09b6fe2c6cb07
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2021-11-22 16:04:15 -0500

    AddItem Functionality added

--- App.js
+++ App.js
@@ -8,8 +8,10 @@ import uuid from 'react-native-uuid';
 //Components 
 import Header from "./components/Header";
 import ListItem from "./components/ListItem";
+import AddItem from "./components/AddItem";
 
 const App = () => {
+  //Items an array of objects used to track shopping list items.
   const [items, setItems] = useState([
     {id: uuid.v4(), text: 'Milk'},
     {id: uuid.v4(), text: 'Eggs'},
@@ -17,15 +19,24 @@ const App = () => {
     {id: uuid.v4(), text: 'Cheese'},
   ]);
 
+  // Used to delete an item, based on id, from the items [{}]
   const deleteItem = (id) => {
     setItems(prevItems => {
       return prevItems.filter(item => item.id != id);
     });
   }
 
+  //Used to add items to the items[{}] state.
+  const addItem = (text) => {
+    setItems(prevItems => {
+      return [{id: uuid.v4(), text}, ...prevItems];
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Header title='Shopping List'/>
+      <AddItem addItem={addItem}/>
       <FlatList
        data={items}
        renderItem={({item}) => (
--- components/AddItem.js
+++ components/AddItem.js
@@ -1,14 +1,45 @@
-import React from "react";
-import {View, Text, StyleSheet} from 'react-native'
+import React, {useState} from "react";
+import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
 
-const AddItem = ({title}) => {
-  return (
-      <View></View>
-  )
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+
+const AddItem = ({addItem}) => {
+    //Use state hook to track text change for adding an item.
+    const [text, setText] = useState('');
+
+    //On change function to update 'text'.
+    const onChange = (textVal) => setText(textVal);
+
+    return (
+        <View>
+            <TextInput placeholder="Add Item..." style={styles.input} onChangeText={onChange}/>
+
+            <TouchableOpacity style={styles.btn} onPress={() => addItem(text)}>
+                <Text style={styles.btnText}>
+                    <Icon name="plus" size={20}/>
+                    Add Item
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-
+    input: {
+        height: 60,
+        padding: 8,
+        fontSize: 16
+    },
+    btn: {
+        backgroundColor: '#c2bad8',
+        padding: 9,
+        margin: 9
+    },
+    btnText: {
+        color: 'darkslateblue',
+        fontSize: 20,
+        textAlign: 'center'
+    }
 })
 
 export default AddItem;
